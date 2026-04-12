177c58446d5711bbeb3c7bdc457a6a22c334ab58
Almi25 <fahmialmirabithah@gmail.com>
2026-04-12 13:50:52 +0700

    Add files via upload

---
+++ Tugas1_PBO_Almi/No2.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class No2 {
+    public static void main(String[] args) {
+        String input = JOptionPane.showInputDialog("Anda sedang belajar apa :");
+        JOptionPane.showMessageDialog(null, "BelajarPemrograman Web sangat mudah:"+ input);
+    }
+}
